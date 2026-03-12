955f9f46a2eb3d85d339d505e9df806698afb688
Daniel Krippner <daniel.krippner@etas.com>
2026-03-12 09:42:50 +0100

    testing

--- scripts/trudag_publish.sh
+++ scripts/trudag_publish.sh
@@ -11,5 +11,6 @@ REPO="${GITHUB_REPOSITORY##*/}"
 
 # Run scoring command, write output to file and capture output at the same time
 echo "Publishing and packaging trudag report"
-trudag publish -a -n "$REPO"
-tar cfj "$TRUDAG_REPORT_ARCHIVE" docs/
+mkdir -p trustable/docs
+trudag publish -a -n "$REPO" -o trustable/docs
+tar cfj "$TRUDAG_REPORT_ARCHIVE" trustable/docs
